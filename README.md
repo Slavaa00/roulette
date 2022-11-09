e4e5e1944a575df608f41772c47eb21f733aff02
Slavaa00 <92999739+Slavaa00@users.noreply.github.com>
2022-11-09 22:09:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Connect wallet to site
+
+Make a bet with value
+
+Win or lose, balances refreshed
+
+Can withdraw balance
+
+Game starting only after certain amount in casino balance (checkupkeep)
---
+++ hardhat.config.js
@@ -0,0 +1,80 @@
+require("@nomicfoundation/hardhat-toolbox")
+
+require("hardhat-deploy")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const GOERLI_RPC_URL =
+	process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "1D9HH172YP1QJ4PY1HIJCTI7FMX9S1NZR9"
+
+const REPORT_GAS = process.env.REPORT_GAS || false
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+	defaultNetwork: "hardhat",
+
+	networks: {
+		hardhat: {
+			chainId: 31337,
+		},
+		localhost: {
+			chainId: 31337,
+		},
+		goerli: {
+			url: GOERLI_RPC_URL,
+			accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+			saveDeployments: true,
+			chainId: 5,
+		},
+	},
+
+	etherscan: {
+		apiKey: {
+			goerli: ETHERSCAN_API_KEY,
+		},
+	},
+
+	gasReporter: {
+		enabled: REPORT_GAS,
+		currency: "USD",
+		outputFile: "gas-report.txt",
+		noColors: true,
+		coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+	},
+
+	contractSizer: {
+		runOnCompile: false,
+		only: ["Roulette"],
+	},
+
+	namedAccounts: {
+		deployer: {
+			default: 0,
+			5: 0,
+		},
+		player: {
+			default: 1,
+			5: 1,
+		},
+	},
+
+	solidity: {
+		compilers: [
+			{
+				version: "0.8.17",
+			},
+			{
+				version: "0.4.24",
+			},
+		],
+	},
+
+	mocha: {
+		timeout: 500000,
+	},
+}
