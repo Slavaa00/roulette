f2bff9feff04bf5f39611ca38077a4d252c2b838
Slavaa00 <92999739+Slavaa00@users.noreply.github.com>
2022-11-10 09:34:46 +0300

    Test Commit

--- contracts/Roulette.sol
+++ contracts/Roulette.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.17;
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+
 import "hardhat/console.sol";
 
 /* Errors */
