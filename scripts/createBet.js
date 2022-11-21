0e3bf756078cc1c51f0703b6119a0ba36cf72c32
Slavaa00 <92999739+Slavaa00@users.noreply.github.com>
2022-11-21 15:28:19 +0300

    2

--- scripts/createBet.js
+++ scripts/createBet.js
@@ -3,7 +3,7 @@ import { ethers } from "hardhat"
 async function createBet() {
     const roulette = await ethers.getContract("Roulette")
     
-    await roulette.createBet(5, [0], {value: ethers.utils.parseEther("0.000000001")})
+    await roulette.createBet(5, [0], {value: ethers.utils.parseEther("0.0000000005")})
     console.log("Entered!")
 }
 
